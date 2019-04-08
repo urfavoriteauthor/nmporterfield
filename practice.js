75e4ab34488d7791105372682c67ce22d601eb8c
URxFAVxAUTHOR <nygel.porterfield@gmail.com>
2019-04-08 02:27:05 -0400

    author site

---
+++ practice.js
@@ -0,0 +1,15 @@
+var numberArray = [];
+var newArray=[];
+var newNum = function(numberArray){return numberArray[i] *2};
+
+var doSomething = function(){
+    for(i=0;i<numberArray.length;i++){
+       numberArray.push(newArray[i] * 2);
+       console.log(numberArray);
+       console.log(newArray);
+    }
+    return newArray;
+    return numberArray;
+};
+
+doSomething();
